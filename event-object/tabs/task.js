ba604492e694ee85dbd3dbb6a0087605a8338a6f
Ayrat Nazmutdinov <nazmut.ayr.97@mail.ru>
2022-07-27 10:52:59 +0300

    Fixed error

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,24 +1,13 @@
-let counter = 0;
-let tabActive = 0;
-let tabContentActive = 0;
-const menu = document.body.querySelectorAll(".tab");
+const tabs = Array.from(document.querySelectorAll(".tab"));
+const tabsContent = Array.from(document.querySelectorAll(".tab__content"));
 
-for (let i = 0; i < menu.length; i++) {
-   menu[i].addEventListener("click", function () {
-      this.classList.add("tab_active");
-      const content = document.body.querySelectorAll('.tab__content');
-      if (counter >= 1) {
-         tabActive.remove("tab_active");
-         tabContentActive.remove("tab__content_active");
-         if (counter > 3) {
-            counter = 0;
-            tabContentActive = 0;
-         }
+const tabsActive = function (event) {
+  let index = tabs.findIndex((tab) => tab.classList.contains("tab_active"));
+  tabs[index].classList.remove("tab_active");
+  tabsContent[index].classList.remove("tab__content_active");
+  event.currentTarget.classList.toggle("tab_active");
+  index = tabs.findIndex((tab) => tab.classList.contains("tab_active"));
+  tabsContent[index].classList.toggle("tab__content_active");
+};
 
-      }
-      content[i].classList.add('tab__content_active');
-      tabContentActive = content[i].classList;
-      tabActive = this.classList;
-      counter += 1;
-   })
-}
+tabs.forEach((tab) => tab.addEventListener("click", tabsActive));
